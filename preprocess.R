ada6bfc209db6c761d7e603605f0c5f0db171f92
Christian Diener <ch.diener@gmail.com>
2018-10-18 11:54:39 -0700

    add steps

--- preprocess.R
+++ preprocess.R
@@ -4,5 +4,6 @@ library(mbtools)
 
 reads <- find_illumina("raw")
 out <- file.path("filtered", basename(reads$forward))
-passed <- filterAndTrim(reads$forward, out, trimLeft=2, maxEE=2, multithread=T)
+passed <- filterAndTrim(file.path("raw", reads$forward), out,
+                        trimLeft=2, maxEE=2, multithread=T)
 fwrite(passed, "preprocess.csv")
