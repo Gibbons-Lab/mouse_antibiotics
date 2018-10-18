bc81a8342b8b6e9d974f8fce52ae0b2ca20bb85a
Christian Diener <ch.diener@gmail.com>
2018-10-18 13:58:33 -0700

    summarize and annotate

--- counts.R
+++ counts.R
@@ -1,8 +1,15 @@
 # Align reads to resistance gene DB and count genes
 
 library(mbtools)
+library(stringr)
 
 reads <- find_illumina("filtered")
+reads$id <- paste0(reads$id, "_", reads$lane)
 alns <- align_bowtie2(reads, "ref/resistance_genes", threads=30)
-counts <- count_hits("alignments")
+counts <- count_hits(alns$alignment)
+counts <- counts[, sum(counts), by=c("seqnames", "sample")]
+ids <- as.character(id(ShortRead::readFasta("../resistance_genes.csv")))
+ids <- as.data.table(str_split(ids, " ", n=2, simplify=T))
+names(ids) <- c("seqnames", "description")
+counts <- split[counts, on="seqnames"]
 fwrite(counts, "counts.csv")
