acd14be2e3ac74a76b4f4ffbb7250d2828d6ce05
Christian Diener <ch.diener@gmail.com>
2018-12-10 16:09:33 -0800

    update analyses

--- preprocess.R
+++ preprocess.R
@@ -4,6 +4,28 @@ library(mbtools)
 
 reads <- find_illumina("raw")
 out <- file.path("filtered", basename(reads$forward))
-passed <- filterAndTrim(reads$forward, out,
-                        trimLeft=2, maxEE=2, multithread=T)
-fwrite(passed, "preprocess.csv")
+quals <- plotQualityProfile(sample(reads$forward, 3), aggregate = TRUE)
+ggsave("figures/qualities.png")
+if (!dir.exists("filtered")) {
+    passed <- filterAndTrim(reads$forward, out,
+                            trimLeft = 5, maxEE = 1, multithread = TRUE)
+    passed$fraction <- passed$reads.out / passed$reads.in
+    fwrite(passed, "preprocess.csv")
+}
+
+
+# Remove mouse and ribosomal reads
+if (!dir.exists("no_host_ribosomal")) {
+refs <- "/proj/gibbons/refs"
+    reads <- find_illumina("filtered")
+    mouse_hits <- filter_reference(reads, "no_mouse",
+                                file.path(refs, "mus_musculus_rna.fna.gz"),
+                                threads = 20)
+    fwrite(mouse_hits, "mouse_counts.csv")
+    reads <- find_illumina("no_mouse")
+    ribo_hits <- filter_reference(reads, "no_host_ribosomal",
+                                  file.path(refs, "silva_dna_132.fna.gz"),
+                                  threads = 20)
+    fwrite(ribo_hits, "ribo_hits.csv")
+    unlink("no_mouse", TRUE)
+}
